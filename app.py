8cfa50dd15a7298f5c61ad589aa3a7e5817b4ddc
Dion Dresschers <dion@dresschers.net>
2021-03-25 09:10:17 +0100

    Used template, and installed Bootstrap 5

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "Hello OpenShebang!"
-
+  pagename = "OpenShebang Home"
+  username = 'Dion'
+  return render_template('index.html', pagename=pagename, username=username)
