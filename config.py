bcb894f5fa872b8140a1a9a30140a2548b8bb0ba
Dion Dresschers <dion@dresschers.net>
2021-05-04 08:53:44 +0200

    lokaal voordat de blog template gewijzigd wordt

--- config.py
+++ config.py
@@ -8,10 +8,11 @@ with open('/etc/py/flask/openshebang.json') as config_file:
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-#    SECRET_KEY = os.environ.get('SECRET_KEY') or 'heel_erg_geheim...NOT'
-    SECRET_KEY = config.get('SECRET_KEY') # Get the info from the JSON-file
+#    If the above `openshebang.json file is NOT used, uncomment both below config settings.`
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'heel_erg_geheim...NOT'
+#    SECRET_KEY = config.get('SECRET_KEY') # Get the info from the JSON-file
+
     SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, config.get('SQLALCHEMY_DATABASE_URI'))
-#    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+ os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # Otherwise always there will be a signal to the application when a change has been made.
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
