d01d1af81ab3026b30d8af363fe52aecb04f0799
Dion Dresschers <dion@dresschers.net>
2023-08-15 12:18:34 +0200

    Added render_template()

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
-from app import app # The first `app` is the directory, the second is the `app` function inside of the `__init__.py`
+from app import app # The `app` first is the directory, the second is the `app` function inside of the `__init__.py`
+from flask import render_template
 
 @app.route('/') # This is a decorator.
 @app.route('/index') # You can chain decorators.
 def index():
-    return "Hello World!"
+  user = {'username': 'Dion'}
+  title = "Main"
+  return render_template('index.html', user=user, title="Home Page") # The first `user` is the variable used in the Jinja2 template, the secons is the Python variable defined.
 
