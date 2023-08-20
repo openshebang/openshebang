2e7ba4dcb34b4fe27fd488dd7f1a02ca662d8b7b
Dion Dresschers <dion@dresschers.net>
2023-08-20 16:58:48 +0200

    Django clean project

--- notes/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here. # As a class. # After the creation of the model class, you run `python3 manage.py makemigrations`
-class Notes(models.Model): # The `model` is imported above, and the Model can be choosen.
-  title = models.CharField(max_length=200)
-  text = models.TextField()
-  created = models.DateTimeField(auto_now_add=True)
-
