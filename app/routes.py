ac5dad3c288f8e240359072a6695643e10750aea
Dion Dresschers <dion@dresschers.net>
2023-12-28 15:03:44 +0100

    Added jinja2 tempaltes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
+from flask import render_template
 from app import app # The first 'app' is the app-Package represents with the dir 'app', in that is a 'app' variable in the __init__.py file.
 
 
 @app.route('/') # This is a decorator, that conntect / to this function
 @app.route('/index') # Second decorator
 def index():
-    return "Hello World!"
+  user = {'username': 'dion'} # Just a user as the db does not exist yet.
+  # return "Hello" + user'username'
+  return render_template('index.html', title='home', user=user) # The first argument is the argument used in the template, the second is what you define it here.
