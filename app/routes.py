e3f5153eae6f4ecb95b48f02d992c5d68c66dd39
Dion Dresschers <dion@dresschers.net>
2023-12-28 15:18:16 +0100

    added jinja2 loops

--- app/routes.py
+++ app/routes.py
@@ -6,5 +6,15 @@ from app import app # The first 'app' is the app-Package represents with the dir
 @app.route('/index') # Second decorator
 def index():
   user = {'username': 'dion'} # Just a user as the db does not exist yet.
+  posts = [ # python list
+    {
+      'author': { 'username': 'John'},
+      'body': 'Hey hello from John',
+    },
+    {
+      'author': { 'username': 'Mimi'},
+      'body': 'Mimimimimi',
+    },
+  ]
   # return "Hello" + user'username'
-  return render_template('index.html', title='home', user=user) # The first argument is the argument used in the template, the second is what you define it here.
+  return render_template('index.html', title='home', user=user, posts=posts) # The first argument is the argument used in the template, the second is what you define it here.
