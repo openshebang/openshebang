a61596854ccce68068dc52e7087133e6bddab0ed
Dion Dresschers <dion@dresschers.net>
2024-01-07 23:59:30 +0100

    2024-01-07: Added config settings 'SHOW_REGISTER_AND_LOG_IN', 'WEBSITE NAME' and added Bootstrap v5.3.2

---
+++ changelog.md
@@ -0,0 +1,3 @@
+Changelog
+
+2024-01-07: Added config settings 'SHOW_REGISTER_AND_LOG_IN', 'WEBSITE NAME' and added Bootstrap v5.3.2 
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,10 +5,13 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
+    app.config.from_mapping( # De config wordt automatisch doorgegeven aan een template
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'osb.sqlite'),
+        WEBSITE_NAME="OpenOnderwijs", # Deze is om de standaard 'flaskr' naam te kunnen veranderen. Dit is ook in de Template aangepast
+        SHOW_REGISTER_AND_LOG_IN=False, # Deze is om de 'Register' en 'Log In' naam al dan niet te tonen, want deze is niet altijd wensbaar.
     )
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
